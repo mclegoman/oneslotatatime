9d88f37473f130e99769a44ddfb669d1ca997529
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-09-27 10:32:12 +1200

    instead of preventing the player from scrolling the hotbar, we instead just set the main hand to slot 0.

--- common/src/main/java/com/mclegoman/oneslotatatime/mixin/PlayerInventoryMixin.java
+++ common/src/main/java/com/mclegoman/oneslotatatime/mixin/PlayerInventoryMixin.java
@@ -1,18 +1,20 @@
 package com.mclegoman.oneslotatatime.mixin;
 
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerInventory.class)
 public abstract class PlayerInventoryMixin {
-	@Shadow public int selectedSlot;
-	@Inject(method = "scrollInHotbar", at = @At("HEAD"), cancellable = true)
-	private void mclmosaat_scrollInHotbar(double scrollAmount, CallbackInfo ci) {
-		ci.cancel();
-		this.selectedSlot = 0;
+	@Shadow @Final public DefaultedList<ItemStack> main;
+	@Inject(method = "getMainHandStack", at = @At("HEAD"), cancellable = true)
+	public void mclmosaat_getMainHandStack(CallbackInfoReturnable<ItemStack> cir) {
+		cir.setReturnValue(this.main.getFirst());
 	}
 }
