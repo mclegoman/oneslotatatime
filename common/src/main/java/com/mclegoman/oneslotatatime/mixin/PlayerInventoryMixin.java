c7b482054cd4fa6e3490a33ebb35d488ebf3a12d
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-09-28 17:42:28 +1200

    selectedSlot should now be 0, most of the time.

--- common/src/main/java/com/mclegoman/oneslotatatime/mixin/PlayerInventoryMixin.java
+++ common/src/main/java/com/mclegoman/oneslotatatime/mixin/PlayerInventoryMixin.java
@@ -8,13 +8,28 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerInventory.class)
 public abstract class PlayerInventoryMixin {
 	@Shadow @Final public DefaultedList<ItemStack> main;
+	@Shadow public int selectedSlot;
+
 	@Inject(method = "getMainHandStack", at = @At("HEAD"), cancellable = true)
 	public void mclmosaat_getMainHandStack(CallbackInfoReturnable<ItemStack> cir) {
+		this.selectedSlot = 0;
 		cir.setReturnValue(this.main.getFirst());
+	}
+	@Inject(method = "swapSlotWithHotbar", at = @At("HEAD"), cancellable = true)
+	public void mclmosaat_swapSlotWithHotbar(int slot, CallbackInfo ci) {
+		this.selectedSlot = 0;
+		this.main.set(0, this.main.get(slot));
+		this.main.set(slot, this.main.getFirst());
+		ci.cancel();
+	}
+	@Inject(method = "getHotbarSize", at = @At("HEAD"), cancellable = true)
+	private static void mclmosaat_getHotbarSize(CallbackInfoReturnable<Integer> cir) {
+		cir.setReturnValue(1);
 	}
 }
